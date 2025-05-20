f60a3f4b025aa5c9b46770628b6cd527d73c2b47
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-05-20 12:04:42 +0200

    cleanup in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,6 @@ import Data.Maybe
 import System.Entropy (getEntropy)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
-import Data.ByteString.Char8 (unpack)
-import Data.Word (Word8)
 import Data.Bits (shiftL, (.|.))
 
 import Primes
@@ -101,24 +99,42 @@ main = do
     entropy <- getEntropy 4
     let g = R.mkStdGen $ if seed' < 0 then byteStringToInt entropy else seed'
     case cmd opts of 
-        PrimeCmd opts -> 
-            let kbits' = kbits opts
-                hex' = hex opts
-                pairFlip = \(a,b) -> (b, a)
-                prime g' = let 
-                    p = randomkbitsPrime g' kbits'
-                    p' = pairFlip <$> p
-                    ps = if hex' then fmap (fmap (`showHex` "")) p' else (fmap show) <$> p'
-                    in fromMaybe (g', "") ps
-                primes = fmap snd $ tail $ take (count' + 1) $ iterate (\(g', _) -> prime g') (g, "")
-            in putStr (unlines primes)
-        StringCmd opts ->
-            let alphabet' = alphabet opts
-                wordLength' = wordLength opts
+        PrimeCmd (PrimeOptions kbits' hex') -> 
+            let 
+                pairFlip (a,b) = (b, a) 
+                -- Pair is a functor with respect to second element
+                -- and since we will be dealing with values of type Just (Integer, g)
+                -- flipping them to obtain Just (g, Integer) will allow us to transformt the Integer value inside Just,
+                -- by using fmap two times - firstly to get inside Maybe, and secondly to tranformt only the second element of Pair.
+                prime :: (R.RandomGen g) => g -> (String, g)
+                prime g' =
+                    let 
+                        p = randomkbitsPrime g' kbits' -- :: Just (Integer, g)
+                        p' = pairFlip <$> p -- :: Just (g, Integer)
+                        toString :: Integer -> String
+                        toString = 
+                            if hex' then
+                                (`showHex` "")
+                            else
+                                show
+                        -- first promote toString with fmap to be able to go inside Maybe values
+                        -- and then apply it with <$> to only the second value of Just
+                        ps = fmap toString <$> p'
+                    in
+                        pairFlip $ fromMaybe (g', "") ps
+                primes = 
+                    fmap fst $ -- :: [Integer]
+                    take count' $ -- :: [(Integer, g)]
+                    iterate (\(_, g') -> prime g') (prime g)
+            in 
+                putStr (unlines primes)
+        StringCmd (StringOptions alphabet' wordLength') ->
+            let 
                 wordGenPairMaybies = 
                     take count' $ 
                     iterate (>>= \(_, g') -> randomWord g' alphabet' wordLength') $
                     randomWord g alphabet' wordLength'
                 wordMaybies = map (fmap fst) wordGenPairMaybies
-                words = map (fromMaybe "") wordMaybies
-            in putStr (unlines words)
+                randomWords = map (fromMaybe "") wordMaybies
+            in 
+                putStr (unlines randomWords)
